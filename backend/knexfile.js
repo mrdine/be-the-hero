88249d9e8238cf23a373cefc778fa6e88163e607
Daniel Gomes <daniel.gomes071@gmail.com>
2020-03-27 14:20:39 -0300

    Adicionado algumas validações e testes

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -14,6 +14,17 @@ module.exports = {
     useNullAsDefault: true,
   },
 
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/test.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
+
   // testar produção com desenvolvedores
   staging: {
     client: 'postgresql',
